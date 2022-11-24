fe646c07364364eb3f8ded0ef2c15e8d2e32695c
tony <292224750@qq.com>
2022-11-24 16:36:03 +0800

    1. NPJ support 2. configurable algorithm selection

--- navtreedata.js
+++ navtreedata.js
@@ -62,8 +62,8 @@ var NAVTREE =
 
 var NAVTREEINDEX =
 [
-"AbstractC20Thread_8h.html",
-"classINTELLI_1_1SPSCQueue.html#a0405df5662782adc890d587122c324c1"
+"",
+"classINTELLI_1_1SPSCQueue.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
