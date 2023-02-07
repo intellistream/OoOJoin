f25100c97539390bf4c9a1ede1dd081480de2de0
rjzhb <rjzhb222@163.com>
2023-02-07 16:44:39 +0800

    fix some coding format problems

--- include/TestBench/TestBench.h
+++ include/TestBench/TestBench.h
@@ -5,6 +5,7 @@
 
 #ifndef INTELLISTREAM_INCLUDE_TESTBENCH_TESTBENCH_H_
 #define INTELLISTREAM_INCLUDE_TESTBENCH_TESTBENCH_H_
+
 #include <Common/Window.h>
 #include <Operator/AbstractOperator.h>
 #include <Utils/IntelliLog.h>
@@ -13,6 +14,7 @@
 #include <iostream>
 #include <sstream>
 #include <TestBench/DataLoaderTable.h>
+
 using namespace INTELLI;
 #define TB_INFO INTELLI_INFO
 #define TB_ERROR INTELLI_ERROR
@@ -31,110 +33,129 @@ namespace OoOJoin {
  * @note Require config if used
  * -"timeStep" U64 The simulation time step in us
  */
-class TestBench {
- protected:
-  void inOrderSort(std::vector<TrackTuplePtr> &arr);
-  void OoOSort(std::vector<TrackTuplePtr> &arr);
-  void inlineTest(void);
-  void forceInOrder(std::vector<TrackTuplePtr> &arr);
-  tsType timeStep = 1;
+    class TestBench {
+    protected:
+        void inOrderSort(std::vector<TrackTuplePtr> &arr);
 
- public:
-  std::vector<TrackTuplePtr> rTuple;
-  std::vector<TrackTuplePtr> sTuple;
-  AbstractOperatorPtr testOp = nullptr;
-  ConfigMapPtr opConfig;
-  size_t AQPResult = 0;
-  TestBench() {}
-  ~TestBench() {}
+        void OoOSort(std::vector<TrackTuplePtr> &arr);
 
-  /**
-   * @brief load a dataset according to the tag
-   * @param tag the name tag of DataLoader
-   * @param globalCfg the global config file to load dataset
-   */
-  void setDataLoader(std::string tag, ConfigMapPtr globalCfg);
-  /**
-   * @brief get the size of loaded s tuple
-   * @return the size
-   */
-  size_t sizeOfS() {
-    return sTuple.size();
-  }
-  /**
-  * @brief get the size of loaded r tuple
-  * @return the size
-  */
-  size_t sizeOfR() {
-    return rTuple.size();
-  }
-  /**
-   * @brief set the dataset to feed
-   * @param _r The r tuples
-   * @param _s The s tuples
-   */
-  void setDataSet(std::vector<TrackTuplePtr> _r, std::vector<TrackTuplePtr> _s);
-  /**
-  * @brief set the operator to test
-  * @param op shared pointer to operator
-   * @param cfg the config file to operator
-   * @return whether the setting is successful
-  */
-  bool setOperator(AbstractOperatorPtr op, ConfigMapPtr cfg);
-  /**
-  * @brief test the operator under in order arrival
-  * @param additionalSort whether or not additionally sort the input
-  * @return the joined result
-  */
-  size_t inOrderTest(bool additionalSort);
-  /**
-  * @brief test the operator under OoO arrival
-  * @param additionalSort whether or not additionally sort the input
-  * @return the joined result
-  */
-  size_t OoOTest(bool additionalSort);
-  /**
-   * @brief print the rTuples to logging system
-   * @param skipZero Whether skip the tuples whose processed time is zero
-   */
-  void logRTuples(bool skipZero = false);
-  /**
-   * @brief save the  rTuples to a file
-   * @param fname the name of file
-   * @param skipZero Whether skip the tuples whose processed time is zero
-   * @return whether the file is written
-   */
-  bool saveRTuplesToFile(string fname, bool skipZero = false);
-  /**
-  * @brief save the  sTuples to a file
-  * @param fname the name of file
-  * @param skipZero Whether skip the tuples whose processed time is zero
-  * @return whether the file is written
-  */
-  bool saveSTuplesToFile(string fname, bool skipZero = false);
-  /**
-   * @brief to compute average latency after run a test
-   * @return the latency in us
-   */
-  double getAvgLatency();
-  /**
-   * @brief to compute the throughput after run a test
-   * @return the throughput in tuples/s
-   */
-  double getThroughput();
-  /**
-   *  @brief to compute the latency t such that fraction of latency is below t
-   *  @param fraction The fraction you want to set
-   * @return the latency in us
-   */
-  double getLatencyPercentage(double fraction);
-  /**
-   * @brief get the break down information of processing time from the join operator tested
-   * @warning should check the nullptr of output
-   * @return The ConfigMapPtr which contains breakdown information, null if no breakdown supported
-   */
-  ConfigMapPtr getTimeBreakDown();
-};
+        void inlineTest();
+
+        void forceInOrder(std::vector<TrackTuplePtr> &arr);
+
+        tsType timeStep = 1;
+
+    public:
+        std::vector<TrackTuplePtr> rTuple;
+        std::vector<TrackTuplePtr> sTuple;
+        AbstractOperatorPtr testOp = nullptr;
+        ConfigMapPtr opConfig;
+        size_t AQPResult = 0;
+
+        TestBench() = default;
+
+        ~TestBench() = default;
+
+        /**
+         * @brief load a dataset according to the tag
+         * @param tag the name tag of DataLoader
+         * @param globalCfg the global config file to load dataset
+         */
+        void setDataLoader(std::string tag, ConfigMapPtr globalCfg);
+
+        /**
+         * @brief get the size of loaded s tuple
+         * @return the size
+         */
+        size_t sizeOfS() const {
+            return sTuple.size();
+        }
+
+        /**
+        * @brief get the size of loaded r tuple
+        * @return the size
+        */
+        size_t sizeOfR() const {
+            return rTuple.size();
+        }
+
+        /**
+         * @brief set the dataset to feed
+         * @param _r The r tuples
+         * @param _s The s tuples
+         */
+        void setDataSet(std::vector<TrackTuplePtr> _r, std::vector<TrackTuplePtr> _s);
+
+        /**
+        * @brief set the operator to test
+        * @param op shared pointer to operator
+         * @param cfg the config file to operator
+         * @return whether the setting is successful
+        */
+        bool setOperator(AbstractOperatorPtr op, ConfigMapPtr cfg);
+
+        /**
+        * @brief test the operator under in order arrival
+        * @param additionalSort whether or not additionally sort the input
+        * @return the joined result
+        */
+        size_t inOrderTest(bool additionalSort);
+
+        /**
+        * @brief test the operator under OoO arrival
+        * @param additionalSort whether or not additionally sort the input
+        * @return the joined result
+        */
+        size_t OoOTest(bool additionalSort);
+
+        /**
+         * @brief print the rTuples to logging system
+         * @param skipZero Whether skip the tuples whose processed time is zero
+         */
+        void logRTuples(bool skipZero = false);
+
+        /**
+         * @brief save the  rTuples to a file
+         * @param fname the name of file
+         * @param skipZero Whether skip the tuples whose processed time is zero
+         * @return whether the file is written
+         */
+        bool saveRTuplesToFile(string fname, bool skipZero = false);
+
+        /**
+        * @brief save the  sTuples to a file
+        * @param fname the name of file
+        * @param skipZero Whether skip the tuples whose processed time is zero
+        * @return whether the file is written
+        */
+        bool saveSTuplesToFile(string fname, bool skipZero = false);
+
+        /**
+         * @brief to compute average latency after run a test
+         * @return the latency in us
+         */
+        double getAvgLatency();
+
+        /**
+         * @brief to compute the throughput after run a test
+         * @return the throughput in tuples/s
+         */
+        double getThroughput();
+
+        /**
+         *  @brief to compute the latency t such that fraction of latency is below t
+         *  @param fraction The fraction you want to set
+         * @return the latency in us
+         */
+        double getLatencyPercentage(double fraction);
+
+        /**
+         * @brief get the break down information of processing time from the join operator tested
+         * @warning should check the nullptr of output
+         * @return The ConfigMapPtr which contains breakdown information, null if no breakdown supported
+         */
+        ConfigMapPtr getTimeBreakDown();
+    };
 /**
  * @}
  */
--- src/Operator/AbstractOperator.cpp
+++ src/Operator/AbstractOperator.cpp
@@ -3,67 +3,73 @@
 //
 
 #include <Operator/AbstractOperator.h>
+
 using namespace OoOJoin;
+
 bool OoOJoin::AbstractOperator::feedTupleS(OoOJoin::TrackTuplePtr ts) {
-  assert(ts);
-  return true;
+    assert(ts);
+    return true;
 }
 
 bool OoOJoin::AbstractOperator::feedTupleR(OoOJoin::TrackTuplePtr tr) {
-  assert(tr);
-  return true;
+    assert(tr);
+    return true;
 }
+
 bool OoOJoin::AbstractOperator::start() {
-  return true;
+    return true;
 }
+
 bool OoOJoin::AbstractOperator::stop() {
-  return true;
+    return true;
 }
+
 size_t OoOJoin::AbstractOperator::getResult() {
-  return 0;
+    return 0;
 }
 
 size_t OoOJoin::AbstractOperator::getAQPResult() {
-  return getResult();
+    return getResult();
 }
 
 bool OoOJoin::AbstractOperator::setConfig(INTELLI::ConfigMapPtr cfg) {
-  config = cfg;
-  if (config == nullptr) {
-    return false;
-  }
-  if (config->existU64("windowLen")) {
-    windowLen = config->getU64("windowLen");
-  }
-  if (config->existU64("slideLen")) {
-    slideLen = config->getU64("slideLen");
-  }
-  if (config->existU64("sLen")) {
-    sLen = config->getU64("sLen");
-  } else {
+    config = cfg;
+    if (config == nullptr) {
+        return false;
+    }
+    if (config->existU64("windowLen")) {
+        windowLen = config->getU64("windowLen");
+    }
+    if (config->existU64("slideLen")) {
+        slideLen = config->getU64("slideLen");
+    }
+    if (config->existU64("sLen")) {
+        sLen = config->getU64("sLen");
+    } else {
 
-    //printf("sLen not set\r\n");
-    OP_ERROR("empty buffer for S stream \n");
+        //printf("sLen not set\r\n");
+        OP_ERROR("empty buffer for S stream \n");
 
-    return false;
-  }
-  if (config->existU64("rLen")) {
-    rLen = config->getU64("rLen");
-  } else {
+        return false;
+    }
+    if (config->existU64("rLen")) {
+        rLen = config->getU64("rLen");
+    } else {
 
-    OP_ERROR("empty buffer for R stream \n");
-    return false;
-  }
-  /*if (config->existU64("timeStep")) {
-    timeStep = config->getU64("timeStep");
-  }
-  else
-  {
-    OP_WARNNING("No setting of timeStep, use 1\n");
-    timeStep=1;
-  }*/
-  return true;
+        OP_ERROR("empty buffer for R stream \n");
+        return false;
+    }
+    /*if (config->existU64("timeStep")) {
+      timeStep = config->getU64("timeStep");
+    }
+    else
+    {
+      OP_WARNNING("No setting of timeStep, use 1\n");
+      timeStep=1;
+    }*/
+    return true;
 }
+
 ConfigMapPtr OoOJoin::AbstractOperator::getTimeBreakDown() {
-  return nullptr;
+    return nullptr;
 }
