09119891359531f750b7830314bbf2581dcde44e
rjzhb <rjzhb222@163.com>
2023-07-08 23:50:16 +0800

    update reproduce for Ubuntu 20.04

--- onKeyReproduce.sh
+++ onKeyReproduce.sh
@@ -1,32 +1,88 @@
-#/bin/bash
-#assume you have already acquired the ubuntu 22.04
-#1. install tool chain
-sudo apt update
-sudo apt install gcc g++ python3 pip -y
-#2. install doxygen for docs
-sudo apt-get install doxygen -y
-sudo apt-get install graphviz -y
-doxygen Doxyfile
-# install lib torch, cpu only
-pip3 install torch==1.13.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-#other oython pkgs dor plotting
+#!/bin/bash
+# check for required packages
+if ! command -v gcc &> /dev/null
+then
+    echo "gcc not found, installing..."
+    sudo apt-get install gcc -y
+fi
+
+if ! command -v g++ &> /dev/null
+then
+    echo "g++ not found, installing..."
+    sudo apt-get install g++ -y
+fi
+
+# check for g++11 and install it if not found
+if ! command -v g++-11 &> /dev/null
+then
+    echo "g++-11 not found, installing..."
+    sudo apt-get install g++-11 -y
+fi
+
+if ! command -v python3 &> /dev/null
+then
+    echo "python3 not found, installing..."
+    sudo apt-get install python3 -y
+fi
+
+if ! command -v pip3 &> /dev/null
+then
+    echo "pip3 not found, installing..."
+    sudo apt-get install python3-pip -y
+fi
+
+# install doxygen
+if ! command -v doxygen &> /dev/null
+then
+    echo "doxygen not found, installing..."
+    sudo apt-get install doxygen -y
+fi
+
+if ! command -v graphviz &> /dev/null
+then
+    echo "graphviz not found, installing..."
+    sudo apt-get install graphviz -y
+fi
+
+# install required Python packages
+
+if [[ "$(lsb_release -rs)" == "22.04" ]]; then
+    pip3 install torch==1.13.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+else
+    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+fi
+
 pip3 install matplotlib pandas numpy
-# build
-mkdir build
+
+
+# build the project
+if [ ! -d "build" ]
+then
+    mkdir build
+fi
 cd build
-cmake -DCMAKE_PREFIX_PATH=`python3 -c 'import torch;print(torch.utils.cmake_prefix_path)'` ..
+    cmake -DCMAKE_PREFIX_PATH=`python3 -c 'import torch;print(torch.utils.cmake_prefix_path)'` -DCMAKE_CXX_COMPILER=g++-11 ..
 make -j24
-# reproduce
-cd benchmark
-mkdir figures
-mkdir results
+
+# run the benchmark and copy the results
+cd ../benchmark
+if [ ! -d "figures" ]
+then
+    mkdir figures
+fi
+if [ ! -d "results" ]
+then
+    mkdir results
+fi
 cd scripts
 ./rerunAll.sh
 cd ../../..
+
+# copy the results and figures to the current directory
 rm -rf results
 rm -rf figures
 cp -r build/benchmark/results .
 cp -r build/benchmark/figures .
-# now, the doc is in doc/html folder by using index.heml
-# figures are in figures.
-# raw results are in results.
+
+# generate documentation
+doxygen Doxyfile
